6d3c2ea3751c5d2a4df5277a509b9be6ed779a73
Richard Cunningham <cunningr@cisco.com>
2026-01-10 08:07:55 +0000

    (revert) move driver code to ./pkg

--- internal/drivers/common/types.go
+++ internal/drivers/common/types.go
@@ -1,42 +1,131 @@
-package common
+package fake
 
 import (
+	"context"
+	"fmt"
+
+	"github.com/cisco/virtual-kubelet-cisco/internal/config"
+	"github.com/cisco/virtual-kubelet-cisco/internal/drivers/common"
+	"github.com/virtual-kubelet/virtual-kubelet/log"
+	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/uuid"
 )
 
-type Container struct {
-	ID          string            `json:"id" yaml:"id"`
-	Name        string            `json:"name" yaml:"name"`
-	Image       string            `json:"image" yaml:"image"`
-	State       ContainerState    `json:"state" yaml:"state"`
-	DeviceID    string            `json:"deviceId" yaml:"deviceId"`
-	NetworkID   string            `json:"networkId" yaml:"networkId"`
-	Resources   ResourceUsage     `json:"resources" yaml:"resources"`
-	Labels      map[string]string `json:"labels" yaml:"labels"`
-	Annotations map[string]string `json:"annotations" yaml:"annotations"`
-	CreatedAt   metav1.Time       `json:"createdAt" yaml:"createdAt"`
-	StartedAt   *metav1.Time      `json:"startedAt,omitempty" yaml:"startedAt,omitempty"`
-	FinishedAt  *metav1.Time      `json:"finishedAt,omitempty" yaml:"finishedAt,omitempty"`
+type FAKEDriver struct {
+	config *config.DeviceConfig
+	pods   []v1.Pod
 }
 
-// ContainerState represents the state of a container
-type ContainerState string
+func NewAppHostingDriver(ctx context.Context, config *config.DeviceConfig) (*FAKEDriver, error) {
+	log.G(ctx).Info("Initialise new FAKE driver")
+	return &FAKEDriver{
+		config: config,
+		pods:   []v1.Pod{},
+	}, nil
 
-const (
-	ContainerStateCreated ContainerState = "created"
-	ContainerStateRunning ContainerState = "running"
-	ContainerStateStopped ContainerState = "stopped"
-	ContainerStateExited  ContainerState = "exited"
-	ContainerStateError   ContainerState = "error"
-	ContainerStateUnknown ContainerState = "unknown"
-)
+}
 
-type ResourceUsage struct {
-	CPU       resource.Quantity `json:"cpu" yaml:"cpu"`
-	Memory    resource.Quantity `json:"memory" yaml:"memory"`
-	Storage   resource.Quantity `json:"storage" yaml:"storage"`
-	NetworkRx int64             `json:"networkRx" yaml:"networkRx"`
-	NetworkTx int64             `json:"networkTx" yaml:"networkTx"`
-	Timestamp metav1.Time       `json:"timestamp" yaml:"timestamp"`
+func (d *FAKEDriver) GetDeviceResources(ctx context.Context) (*v1.ResourceList, error) {
+
+	log.G(ctx).Info("Pod GetDeviceResources request received")
+	resources := v1.ResourceList{
+		v1.ResourceCPU:     resource.MustParse("8"),
+		v1.ResourceMemory:  resource.MustParse("16Gi"),
+		v1.ResourceStorage: resource.MustParse("100Gi"),
+		v1.ResourcePods:    resource.MustParse("16"),
+	}
+
+	return &resources, nil
+}
+
+func (d *FAKEDriver) DeployContainer(ctx context.Context, pod *v1.Pod) error {
+	// Convert K8s pod name to valid Cisco AppHosting name
+	appName := common.K8sToAppHostingName(pod.Namespace, pod.Name)
+	log.G(ctx).WithFields(log.Fields{
+		"namespace":   pod.Namespace,
+		"pod":         pod.Name,
+		"appHostName": appName,
+	}).Info("Pod DeployContainer request received")
+
+	// Update pod status
+	now := metav1.Now()
+	pod.Status = v1.PodStatus{
+		Phase:     v1.PodRunning,
+		HostIP:    "1.1.1.2",
+		PodIP:     "1.1.1.1",
+		StartTime: &now,
+		Conditions: []v1.PodCondition{
+			{
+				Type:               v1.PodInitialized,
+				Status:             v1.ConditionTrue,
+				LastTransitionTime: now,
+			},
+			{
+				Type:               v1.PodReady,
+				Status:             v1.ConditionTrue,
+				LastTransitionTime: now,
+			},
+			{
+				Type:               v1.PodScheduled,
+				Status:             v1.ConditionTrue,
+				LastTransitionTime: now,
+			},
+		},
+	}
+
+	for _, container := range pod.Spec.Containers {
+		containerStatus := v1.ContainerStatus{
+			Name:  container.Name,
+			Image: container.Image,
+			Ready: true,
+			State: v1.ContainerState{
+				Running: &v1.ContainerStateRunning{
+					StartedAt: metav1.Now(),
+				},
+			},
+			ContainerID: string(uuid.NewUUID()),
+		}
+		pod.Status.ContainerStatuses = append(pod.Status.ContainerStatuses, containerStatus)
+	}
+	d.pods = append(d.pods, *pod)
+	log.G(ctx).WithFields(log.Fields{
+		"pod": pod.Name,
+	}).Info("Stored pod in FAKEDriver")
+	return nil
+}
+
+func (d *FAKEDriver) UpdateContainer(ctx context.Context, pod *v1.Pod) error {
+	// TODO
+	log.G(ctx).Info("Pod UpdateContainer request received")
+	return nil
+}
+
+func (d *FAKEDriver) StopAndRemoveContainer(ctx context.Context, pod *v1.Pod) error {
+	log.G(ctx).WithFields(log.Fields{
+		"pod": pod.Name,
+	}).Info("Pod StopAndRemoveContainer request received")
+	return nil
+}
+
+func (d *FAKEDriver) GetContainerStatus(ctx context.Context, namespace, name string) (*v1.Pod, error) {
+	// TODO
+	log.G(ctx).WithFields(log.Fields{
+		"namespace": namespace,
+		"pod":       name,
+	}).Info("Looking for pod")
+	pod := common.FindPod(d.pods, namespace, name)
+	if pod != nil {
+		return pod, nil
+	}
+
+	log.G(ctx).Info("FAKEDriver couldn't fnd pod")
+	return nil, fmt.Errorf("could not find pod: %s, %s", namespace, name)
+}
+
+func (d *FAKEDriver) ListContainers(ctx context.Context) ([]*v1.Pod, error) {
+	// TODO
+	log.G(ctx).Info("Pod ListContainers request received")
+	return nil, nil
 }
