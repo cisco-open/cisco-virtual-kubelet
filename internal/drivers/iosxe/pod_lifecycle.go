09dabef1f81c0ab17f164cb85f78627d589b6e47
Richard Cunningham <cunningr@cisco.com>
2026-01-23 20:34:47 +0000

    Add container resource mapping

--- internal/drivers/iosxe/pod_lifecycle.go
+++ internal/drivers/iosxe/pod_lifecycle.go
@@ -3,11 +3,13 @@ package iosxe
 import (
 	"context"
 	"fmt"
+	"net"
 
 	"github.com/cisco/virtual-kubelet-cisco/internal/drivers/common"
 	"github.com/openconfig/ygot/ygot"
 	"github.com/virtual-kubelet/virtual-kubelet/log"
 	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
 )
 
 func (d *XEDriver) ConfigureAppContainer(ctx context.Context, pod *v1.Pod) error {
@@ -28,12 +30,13 @@ func (d *XEDriver) ConfigureAppContainer(ctx context.Context, pod *v1.Pod) error
 			return fmt.Errorf("failed to create app struct for container %s: %w", container.Name, err)
 		}
 
+		netConfig := d.getNetworkConfig(pod, &container)
 		gapp.ApplicationNetworkResource = &Cisco_IOS_XEAppHostingCfg_AppHostingCfgData_Apps_App_ApplicationNetworkResource{
-			ManagementInterfaceName:                        ygot.String("0"),
-			ManagementGuestIpAddress:                       ygot.String("1.1.1.10"),
-			ManagementGuestIpNetmask:                       ygot.String("255.255.255.0"),
-			VirtualportgroupApplicationDefaultGateway_1:    ygot.String("1.1.1.1"),
-			VirtualportgroupGuestInterfaceDefaultGateway_1: ygot.Uint8(0),
+			ManagementInterfaceName:                        ygot.String(netConfig.managementInterface),
+			ManagementGuestIpAddress:                       ygot.String(netConfig.managementIP),
+			ManagementGuestIpNetmask:                       ygot.String(netConfig.managementNetmask),
+			VirtualportgroupApplicationDefaultGateway_1:    ygot.String(netConfig.defaultGateway),
+			VirtualportgroupGuestInterfaceDefaultGateway_1: ygot.Uint8(netConfig.gatewayInterface),
 		}
 
 		gapp.RunOptss = &Cisco_IOS_XEAppHostingCfg_AppHostingCfgData_Apps_App_RunOptss{
@@ -54,12 +57,13 @@ func (d *XEDriver) ConfigureAppContainer(ctx context.Context, pod *v1.Pod) error
 			},
 		}
 
+		resConfig := d.getResourceConfig(&container)
 		gapp.ApplicationResourceProfile = &Cisco_IOS_XEAppHostingCfg_AppHostingCfgData_Apps_App_ApplicationResourceProfile{
 			ProfileName:      ygot.String("custom"),
-			CpuUnits:         ygot.Uint16(1000),
-			MemoryCapacityMb: ygot.Uint16(512),
-			DiskSizeMb:       ygot.Uint16(1024),
-			Vcpu:             ygot.Uint16(2),
+			CpuUnits:         ygot.Uint16(resConfig.cpuUnits),
+			MemoryCapacityMb: ygot.Uint16(resConfig.memoryMB),
+			DiskSizeMb:       ygot.Uint16(resConfig.diskMB),
+			Vcpu:             ygot.Uint16(resConfig.vcpu),
 		}
 
 		gapp.Start = ygot.Bool(true)
@@ -73,4 +77,122 @@ func (d *XEDriver) ConfigureAppContainer(ctx context.Context, pod *v1.Pod) error
 	}
 
 	return nil
+}
+
+type networkConfig struct {
+	managementInterface string
+	managementIP        string
+	managementNetmask   string
+	defaultGateway      string
+	gatewayInterface    uint8
+}
+
+func (d *XEDriver) getNetworkConfig(pod *v1.Pod, container *v1.Container) *networkConfig {
+	ip, netmask, gateway := d.allocateIPForContainer(pod, container)
+	
+	return &networkConfig{
+		managementInterface: "0",
+		managementIP:        ip,
+		managementNetmask:   netmask,
+		defaultGateway:      gateway,
+		gatewayInterface:    0,
+	}
+}
+
+func (d *XEDriver) allocateIPForContainer(pod *v1.Pod, container *v1.Container) (ip, netmask, gateway string) {
+	if d.config.Networking.PodCIDR != "" {
+		_, ipNet, err := net.ParseCIDR(d.config.Networking.PodCIDR)
+		if err == nil {
+			netmask = net.IP(ipNet.Mask).String()
+			gateway = d.getGatewayFromCIDR(ipNet)
+			containerIndex := d.getContainerIndex(pod, container)
+			ip = d.getIPForContainer(ipNet, containerIndex)
+			return
+		}
+	}
+	
+	return "1.1.1.10", "255.255.255.0", "1.1.1.1"
+}
+
+func (d *XEDriver) getContainerIndex(pod *v1.Pod, container *v1.Container) int {
+	for i, c := range pod.Spec.Containers {
+		if c.Name == container.Name {
+			return i
+		}
+	}
+	return 0
+}
+
+func (d *XEDriver) getGatewayFromCIDR(ipNet *net.IPNet) string {
+	ip := ipNet.IP.To4()
+	if ip == nil {
+		return ""
+	}
+	ip[3] = ip[3] + 1
+	return ip.String()
+}
+
+func (d *XEDriver) getIPForContainer(ipNet *net.IPNet, containerIndex int) string {
+	ip := ipNet.IP.To4()
+	if ip == nil {
+		return ""
+	}
+	ip[3] = ip[3] + uint8(10+containerIndex)
+	return ip.String()
+}
+
+type resourceConfig struct {
+	cpuUnits uint16
+	memoryMB uint16
+	diskMB   uint16
+	vcpu     uint16
+}
+
+func (d *XEDriver) getResourceConfig(container *v1.Container) *resourceConfig {
+	config := &resourceConfig{
+		cpuUnits: 1000,
+		memoryMB: 512,
+		diskMB:   1024,
+		vcpu:     1,
+	}
+	
+	if container.Resources.Requests != nil {
+		if cpu := container.Resources.Requests.Cpu(); cpu != nil && !cpu.IsZero() {
+			config.cpuUnits = uint16(cpu.MilliValue())
+		}
+		if mem := container.Resources.Requests.Memory(); mem != nil && !mem.IsZero() {
+			config.memoryMB = uint16(mem.Value() / (1024 * 1024))
+		}
+		if storage := container.Resources.Requests.Storage(); storage != nil && !storage.IsZero() {
+			config.diskMB = uint16(storage.Value() / (1024 * 1024))
+		}
+	}
+	
+	if container.Resources.Limits != nil {
+		if cpu := container.Resources.Limits.Cpu(); cpu != nil && !cpu.IsZero() {
+			milliCores := cpu.MilliValue()
+			config.vcpu = uint16((milliCores + 999) / 1000)
+			if config.vcpu < 1 {
+				config.vcpu = 1
+			}
+		}
+	}
+	
+	if d.config.ResourceLimits.DefaultCPU != "" {
+		if q, err := resource.ParseQuantity(d.config.ResourceLimits.DefaultCPU); err == nil {
+			config.cpuUnits = uint16(q.MilliValue())
+		}
+	}
+	if d.config.ResourceLimits.DefaultMemory != "" {
+		if q, err := resource.ParseQuantity(d.config.ResourceLimits.DefaultMemory); err == nil {
+			config.memoryMB = uint16(q.Value() / (1024 * 1024))
+		}
+	}
+	if d.config.ResourceLimits.DefaultStorage != "" {
+		if q, err := resource.ParseQuantity(d.config.ResourceLimits.DefaultStorage); err == nil {
+			config.diskMB = uint16(q.Value() / (1024 * 1024))
+		}
+	}
+	
+	return config
 }
