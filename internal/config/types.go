3f236757fa9d7ba65ca28940dc26c300706de902
Morten Skriver <moskrive@cisco.com>
2026-01-29 20:27:48 +0100

    Add license

--- internal/config/types.go
+++ internal/config/types.go
@@ -1,3 +1,16 @@
+// Copyright © 2026 Cisco Systems, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 package config
 
 import (
@@ -81,10 +94,10 @@ type NetworkConfig struct {
 	DHCPEnabled      bool               `mapstructure:"dhcpEnabled"`
 	VirtualPortGroup string             `mapstructure:"virtualPortGroup"`
 	ServiceCIDR      string             `mapstructure:"serviceCIDR"`
-	DNSServers     []string           `mapstructure:"dnsServers"`
-	VLANRange      VLANRange          `mapstructure:"vlanRange"`
-	SecurityGroups []string           `mapstructure:"securityGroups,omitempty"`
-	LoadBalancer   LoadBalancerConfig `mapstructure:"loadBalancer"`
+	DNSServers       []string           `mapstructure:"dnsServers"`
+	VLANRange        VLANRange          `mapstructure:"vlanRange"`
+	SecurityGroups   []string           `mapstructure:"securityGroups,omitempty"`
+	LoadBalancer     LoadBalancerConfig `mapstructure:"loadBalancer"`
 }
 
 // VLANRange represents VLAN ID range for pod isolation
--- tests/yang/scripts/rpc_handler.py
+++ tests/yang/scripts/rpc_handler.py
@@ -1,3 +1,17 @@
+# Copyright © 2026 Cisco Systems, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http:#www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import sysrepo
 import signal
 import xml.etree.ElementTree as ET
@@ -91,7 +105,7 @@ def update_xml_file_directly(app_name, new_status, file_path):
         root = tree.getroot()
 
         # 2. Wildcard search handles any existing prefix issues during lookup
-        xpath_query = f".//{{*}}app[{{*}}name='{app_name}']/{{*}}details/{{*}}state"
+        xpath_query = f".#{{*}}app[{{*}}name='{app_name}']/{{*}}details/{{*}}state"
         state_node = root.find(xpath_query)
 
         if state_node is not None:
